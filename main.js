5b89e15d603b3acabf0f3ecc57bccf7f9ff5cc8e
david toska <david.toska@outlook.com>
2020-07-07 11:37:40 +0200

    can load url from cli-command

--- main.js
+++ main.js
@@ -2,40 +2,50 @@ const { app, BrowserWindow } = require("electron");
 const url = require("url");
 const path = require("path");
 
-function createWindow() {
-  // Create the browser window.
+function createLocalFileWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       nodeIntegration: false, // is default value after Electron v5
       contextIsolation: true, // protect against prototype pollution
       enableRemoteModule: false, // turn off remote
-      preload: path.join(__dirname, "preload.js"), // use a preload script
     },
     kiosk: true,
+    fullscreen: true,
   });
 
   const indexPath = path.join(__dirname, `/index.html`);
-  win
-    .loadURL(
-      url.format({
-        pathname: indexPath,
-        protocol: "file:",
-        slashes: true,
-      })
-    )
-    .then((res) => {
-      console.log("URL loaded", indexPath);
-    })
-    .catch((err) => {
-      console.error("Could not load url", indexPath);
-    });
+  const argsUrls = [...process.argv].filter((a) => a.includes("url=http"));
 
-  win.webContents.openDevTools();
+  if (argsUrls.length === 1) {
+    const argsUrl = argsUrls[0].split("=")[1].trim();
+    win
+      .loadURL(argsUrl)
+      .then(() => {
+        console.log("Url loaded: " + argsUrl);
+      })
+      .catch((err) => {
+        console.log("Error while loading url: " + argsUrl);
+        console.log(err);
+      });
+  } else {
+    win
+      .loadURL(
+        url.format({
+          pathname: indexPath,
+          protocol: "file:",
+          slashes: true,
+        })
+      )
+      .then((res) => {
+        console.log("URL loaded", indexPath);
+      })
+      .catch((err) => {
+        console.error("Could not load url", indexPath);
+      });
+  }
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createLocalFileWindow);
 
 app.on("window-all-closed", () => {
   app.quit();
--- preload.js
+++ preload.js
@@ -1,1 +1,8 @@
 console.log("preload script is running.");
+const argsUrls = [...process.argv].filter((a) => a.includes("url="));
+if (argsUrls.length === 1) {
+  const url = argsUrls[0].split("=")[1].trim();
+  console.log(url);
+}
+console.log(argsUrls);
+console.log(process.argv);
