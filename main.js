b294efc2096ec97562442866f85b92c4583b7528
davidtoska <david.toska@outlook.com>
2022-06-04 23:17:03 +0200

    updated depenedncies

--- main.js
+++ main.js
@@ -1,51 +1,53 @@
 const { app, BrowserWindow } = require("electron");
-const url = require("url");
-const path = require("path");
 
-function createLocalFileWindow() {
-  const win = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: false, // is default value after Electron v5
-      contextIsolation: true, // protect against prototype pollution
-      enableRemoteModule: false, // turn off remote
-    },
-    kiosk: true,
-    fullscreen: true,
-  });
+app
+  .whenReady()
+  .then(() => {
+    const win = new BrowserWindow({
+      webPreferences: {
+        nodeIntegration: false, // is default value after Electron v5
+        contextIsolation: true, // protect against prototype pollution
+        enableRemoteModule: false, // turn off remote
+      },
 
-  const indexPath = path.join(__dirname, `/index.html`);
-  const argsUrls = [...process.argv].filter((a) => a.includes("url=http"));
+      kiosk: true,
+      fullscreen: true,
+    });
 
-  if (argsUrls.length === 1) {
-    const argsUrl = argsUrls[0].split("=")[1].trim();
-    win
-      .loadURL(argsUrl)
-      .then(() => {
-        console.log("Url loaded: " + argsUrl);
-      })
-      .catch((err) => {
-        console.log("Error while loading url: " + argsUrl);
-        console.log(err);
-      });
-  } else {
-    win
-      .loadURL(
-        url.format({
-          pathname: indexPath,
-          protocol: "file:",
-          slashes: true,
+    const argsUrls = [...process.argv].filter((a) => a.includes("url=http"));
+
+    if (argsUrls.length === 1) {
+      const spt_index = argsUrls[0].indexOf("=");
+      const argsUrl = argsUrls[0].substring(spt_index + 1).trim();
+      win
+        .loadURL(argsUrl)
+        .then(() => {
+          console.log("Url loaded: " + argsUrl);
         })
-      )
-      .then((res) => {
-        console.log("URL loaded", indexPath);
-      })
-      .catch((err) => {
-        console.error("Could not load url", indexPath);
-      });
-  }
-}
-
-app.whenReady().then(createLocalFileWindow);
+        .catch((err) => {
+          console.error("Error while loading url: " + argsUrl);
+          console.error(err);
+        });
+    } else {
+      win
+        .loadFile("index.html")
+        .then(() => {
+          console.log("Did load empty information page");
+          console.log("No url given to web-kiosk");
+          console.log(
+            "Use this format when running program: web-kiosk url=https://google.com"
+          );
+        })
+        .catch((err) => {
+          console.error("Fatal error. Could not load empty information page.");
+          console.error(err);
+        });
+    }
+  })
+  .catch((err) => {
+    console.error("Unknown error wile running web-kiosk.");
+    console.error(err);
+  });
 
 app.on("window-all-closed", () => {
   app.quit();
--- preload.js
+++
@@ -1,8 +0,0 @@
-console.log("preload script is running.");
-const argsUrls = [...process.argv].filter((a) => a.includes("url="));
-if (argsUrls.length === 1) {
-  const url = argsUrls[0].split("=")[1].trim();
-  console.log(url);
-}
-console.log(argsUrls);
-console.log(process.argv);
